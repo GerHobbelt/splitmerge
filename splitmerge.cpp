88ce16cb1df7aa3ce2a8cddc6063e2a252ec8ad3
Ger Hobbelt <ger@hobbelt.com>
2023-01-28 02:19:22 +0100

    implemented split functionality.

--- splitmerge.cpp
+++ splitmerge.cpp
@@ -1,8 +1,24 @@
 
-#include <stdio.h>
-#include <stdlib.h>
 
-#include <iostream>
+#include "mupdf/mutool.h"
+#include "mupdf/fitz.h"
+#include "mupdf/helpers/dir.h"
+#include "mupdf/helpers/system-header-files.h"
+
+#include "blake3.h"
+#include "endianness.h"
+
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#if defined(_WIN32)
+#include <windows.h>
+#endif
+
+#undef MIN
+#define MIN(a, b)  ((a) <= (b) ? (a) : (b))
 
 
 static int usage()
@@ -100,30 +116,320 @@ splitmerge -k largeFile.bin.spec0000
 kept, alongside the reconstructed largeFile.bin file.
 
 )xxx";
-	fprintf(stderr, "%s", msg);
+	fz_info(fz_get_global_context(), "%s", msg);
 	return EXIT_FAILURE;
 }
+
+
+#if BLAKE3_OUT_LEN != 32
+#error "B0RK! BLAKE3 hashes MUST be 32 bytes wide! This is WRONG, man! W-w-w-wrong!!!1!"
+#endif
 
 
 // stored on disk in Little Endian format:
 typedef struct info_header
 {
-	int64_t format_version;
-	int64_t filesize;
+	int32_t format_version;
+	int32_t segment_number;    // index number of this segment; starts with 1 (!)
+	uint64_t filesize;
 	uint8_t hashbytes[32];
 
 	// reserved for future use.
-	uint8_t unused[256 - 32 - 8];
+	uint8_t unused[256 - 32 - 16];
 } info_header_t;
 
 
-int main(int argc, const char **argv)
+
+
+
+
+#ifdef _MSC_VER
+#define main main_utf8
+#endif
+
+int main(int argc, const char** argv)
 {
-	if (argc <= 1)
-		return usage();
+	fz_context* ctx = NULL;
+
+	if (!fz_has_global_context())
+	{
+		ctx = fz_new_context(NULL, NULL, FZ_STORE_UNLIMITED);
+		if (!ctx)
+		{
+			fz_error(ctx, "cannot initialise MuPDF context");
+			return EXIT_FAILURE;
+		}
+		fz_set_global_context(ctx);
+	}
+
+	ctx = fz_new_context(NULL, NULL, FZ_STORE_DEFAULT);
+	if (!ctx)
+	{
+		fz_error(ctx, "cannot initialise MuPDF context");
+		return EXIT_FAILURE;
+	}
+
+	if (argc == 0)
+	{
+		fz_error(ctx, "No command name found!");
+		return EXIT_FAILURE;
+	}
 
 	bool keep = false;
+	bool split_forced = false;
 	int64_t split_size = 15000000;
+
+	int c;
+	int cli_count = 0;
+
+	fz_getopt_reset();
+	while ((c = fz_getopt(argc, argv, "hskl:")) != -1)
+	{
+		cli_count++;
+		switch (c)
+		{
+		case 'l': {
+			char *unit_str = NULL;
+			split_size = strtol(fz_optarg, &unit_str, 10);
+			switch (toupper(*unit_str))
+			{
+			case 0:
+				break;
+
+			case 'B':
+				unit_str++;
+				break;
+
+			case 'K':
+				split_size *= 1024;
+				unit_str++;
+				break;
+
+			case 'M':
+				split_size += 1024 * 1024;
+				unit_str++;
+				break;
+
+			default:
+				break;
+			}
+			if (*unit_str)
+			{
+				fz_error(ctx, "split-size unit MUST be B, K or M. Unsupported unit: %s\n", unit_str);
+				return EXIT_FAILURE;
+			}
+		}
+			break;
+
+		case 'k': keep = true; break;
+		case 's': split_forced = true; break;
+		case 'h':
+		default:
+			return usage();
+		}
+	}
+
+	if (fz_optind >= argc)
+	{
+		if (cli_count)
+		{
+			fz_error(ctx, "ERROR: No source file(s) specified to split.\n\n\n");
+		}
+		return usage();
+	}
+
+	int count_done = 0;
+	int count_skipped = 0;
+
+	while (fz_optind < argc)
+	{
+		const char *filepath = argv[fz_optind++];
+
+		const char *filename = fz_basename(filepath);
+		const char *file_ext = fz_name_extension(filename);
+
+		// we accept any of the segments' filenames, hence we must convert its extension to
+		// properly match .seg0000 :
+		// 
+		// ..SEG1234 --> .SEG0000
+		char *ext2test = fz_strdup(ctx, file_ext);
+		if (0 == fz_strncasecmp(file_ext, ".seg0000", 4))
+		{
+			char *p = ext2test + strlen(ext2test) - 1;
+			for (; p >= ext2test && isdigit(*p); p--)
+				*p = '0';
+			// SEG123456789 --> SEG1234 ~ SEG0000
+			if (p - ext2test < 5 && strlen(ext2test) > 8)
+				ext2test[8] = 0;
+		}
+
+		// split or merge?
+		bool split = (split_forced || 0 != fz_strcasecmp(ext2test, ".seg0000"));
+
+		fz_free(ctx, ext2test);
+
+		if (split)
+		{
+			static fz_stream* datafeed = NULL;
+			char *seg_filepath = NULL;
+			fz_output *dest_file = NULL;
+
+			fz_try(ctx)
+			{
+				struct stat st ={0};
+				if (stat(filepath, &st))
+				{
+					int err = errno;
+					fz_throw(ctx, FZ_ERROR_GENERIC, "Cannot inspect source file %q: error %d (%s)\n", filepath, err, strerror(err));
+				}
+
+				uint64_t filesize = st.st_size;
+
+				// no need to split when the file is smaller than the specified segment size!
+				if (filesize <= split_size)
+				{
+					fz_info(ctx, "No need to split file %q as it would only occupy a single segment anyway: filesize: %zu vs. segment size: %zu.\n", filepath, (size_t)filesize, (size_t)split_size);
+
+					count_done++;
+					count_skipped++;
+				} else
+				{
+					datafeed = fz_open_file(ctx, filepath);
+					if (datafeed == NULL)
+					{
+						int err = errno;
+						fz_throw(ctx, FZ_ERROR_GENERIC, "Cannot open source file %q: error %d (%s)\n", filepath, err, strerror(err));
+					}
+
+					// calculate the hash/fingerprint of the source file:
+
+					// Initialize the hasher.
+					blake3_hasher hasher;
+					blake3_hasher_init(&hasher);
+
+					// Read input bytes from file.
+					unsigned char buf[65536];
+					size_t n = fz_read(ctx, datafeed, buf, sizeof(buf));
+					while (n > 0)
+					{
+						blake3_hasher_update(&hasher, buf, n);
+						n = fz_read(ctx, datafeed, buf, sizeof(buf));
+					}
+
+					// Finalize the hash. BLAKE3_OUT_LEN is the default output length, 32 bytes.
+					uint8_t hash[BLAKE3_OUT_LEN];
+					blake3_hasher_finalize(&hasher, hash, BLAKE3_OUT_LEN);
+
+					fz_seek(ctx, datafeed, 0, SEEK_SET);
+
+					// prep a header block:
+					int segment_index = 0;
+
+					int64_t copy_len = filesize;
+					while (copy_len > 0)
+					{
+						info_header_t seg_hdr =
+						{
+							1,		// version
+							segment_index + 1,   // segment number in the file header is 1-based on purpose!
+							filesize,
+						};
+						memcpy(seg_hdr.hashbytes, hash, BLAKE3_OUT_LEN);
+
+						// construct filename for each segment file:
+						seg_filepath = fz_asprintf(ctx, "%s.seg%04d", filepath, segment_index);
+						if (!seg_filepath)
+						{
+							fz_throw(ctx, FZ_ERROR_ABORT, "Out of memory while setting up segment file(s) for source file %q\n", filepath);
+						}
+
+						dest_file = fz_new_output_with_path(ctx, seg_filepath, FALSE);
+
+#if !defined(L_LITTLE_ENDIAN)
+						fz_write_int32_le(ctx, dest_file, 1);
+						fz_write_int32_le(ctx, dest_file, segment_index);
+						fz_write_int64_le(ctx, dest_file, filesize);
+						fz_write_data(ctx, dest_file, hash, BLAKE3_OUT_LEN);
+						uint8_t reserved_nuls[256 - 32 - 16] ={0};
+						fz_write_data(ctx, dest_file, reserved_nuls, sizeof(reserved_nuls));
+#else
+						fz_write_data(ctx, dest_file, &seg_hdr, sizeof(seg_hdr));
+#endif
+
+						// now write the segment's data:
+						size_t segment_size = MIN(split_size, copy_len);
+						size_t seg_copy_size = segment_size;
+						size_t read_size = MIN(sizeof(buf), seg_copy_size);
+						size_t n = fz_read(ctx, datafeed, buf, read_size);
+						while (n > 0)
+						{
+							fz_write_data(ctx, dest_file, buf, n);
+							seg_copy_size -= n;
+							read_size = MIN(sizeof(buf), seg_copy_size);
+							if (read_size > 0)
+								n = fz_read(ctx, datafeed, buf, read_size);
+							else
+								n = 0;
+						}
+
+						// has the entire designated segment been copied?
+						if (seg_copy_size != 0)
+						{
+							fz_throw(ctx, FZ_ERROR_ABORT, "Failed to complete writing to segment file %q; aborting!\n", seg_filepath);
+						}
+
+						fz_close_output(ctx, dest_file);
+						fz_drop_output(ctx, dest_file);
+						dest_file = NULL;
+
+						copy_len -= segment_size;
+						segment_index++;
+					}
+
+					// all done: close source file
+					fz_drop_stream(ctx, datafeed);
+					datafeed = NULL;
+				}
+			}
+			fz_catch(ctx)
+			{
+				if (datafeed)
+				{
+					fz_drop_stream(ctx, datafeed);
+					datafeed = NULL;
+				}
+
+				if (dest_file)
+				{
+					fz_drop_output(ctx, dest_file);
+					dest_file = NULL;
+				}
+
+				fz_error(ctx, "Failure while splitting %q: %s", filepath, fz_caught_message(ctx));
+			}
+		}
+		else
+		{
+			// MERGE
+
+		}
+
+		fz_flush_warnings(ctx);
+	}
+
+	fz_drop_context(ctx);
 
 	return EXIT_SUCCESS;
 }
+
+#ifdef _MSC_VER
+int wmain(int argc, const wchar_t *wargv[])
+{
+	const char **argv = (const char **)fz_argv_from_wargv(argc, wargv);
+	if (!argv)
+		return EXIT_FAILURE;
+	int ret = main(argc, argv);
+	fz_free_argv(argc, argv);
+	return ret;
+}
+#endif
